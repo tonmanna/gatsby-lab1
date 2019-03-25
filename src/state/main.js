419e58b29f02c5e7e04f7c24fb5c1d710d380b1e
Worawut <tonmanna@gmail.com>
2019-03-25 20:59:06 +0700

    last commit

--- src/state/main.js
+++ src/state/main.js
@@ -10,5 +10,21 @@ import rootSaga from './sagas'
 const sagaMiddleware = createSagaMiddleware()
 const store = createStore(reducer, applyMiddleware(sagaMiddleware))
 sagaMiddleware.run(rootSaga)
+
 const action = type => store.dispatch({ type })
+
+function render() {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => action('INCREMENT')}
+      onDecrement={() => action('DECREMENT')}
+      onIncrementIfOdd={() => action('INCREMENT_IF_ODD')}
+      onIncrementAsync={() => action('INCREMENT_ASYNC')}
+    />,
+    document.getElementById('root'),
+  )
+}
+
+render()
 store.subscribe(render)
